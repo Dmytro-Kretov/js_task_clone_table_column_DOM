49d9a2c85e76072127ec0b1f378e61a2bbae258e
Dmytro Kretov <dmytro.kretov@gmail.com>
2024-10-29 13:56:11 +0200

    make build

---
+++ index.68f8b10f.js
@@ -0,0 +1,2 @@
+var e=document.querySelector("table"),l=e.querySelectorAll("thead tr"),t=e.querySelectorAll("tbody tr"),r=e.querySelectorAll("tfoot tr");function o(e){e.forEach(function(e){var l=e.cells[1].cloneNode(!0);e.insertBefore(l,e.cells[e.cells.length-1])})}o(l),o(t),o(r);
+//# sourceMappingURL=index.68f8b10f.js.map
---
+++ index.f75de5e1.js
@@ -0,0 +1,16 @@
+"use strict";
+const table = document.querySelector("table");
+const theadRows = table.querySelectorAll("thead tr");
+const tbodyRows = table.querySelectorAll("tbody tr");
+const tfootRows = table.querySelectorAll("tfoot tr");
+function cloneColumn(rows) {
+    rows.forEach((row)=>{
+        const cellToClone = row.cells[1].cloneNode(true);
+        row.insertBefore(cellToClone, row.cells[row.cells.length - 1]);
+    });
+}
+cloneColumn(theadRows);
+cloneColumn(tbodyRows);
+cloneColumn(tfootRows);
+
+//# sourceMappingURL=index.f75de5e1.js.map
